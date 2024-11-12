200130ed88ef978793fcbee080b79806b30b4184
oViniciusElias <vinicius.elias@unifesp.br>
2024-11-12 11:17:00 -0300

    Add files via upload

---
+++ 1005.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main(){
+
+    double a, b, c;
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+
+    c=((a*3.5)+(b*7.5))/11.0;
+
+    printf("MEDIA = %.5lf\n", c);
+    return 0;
+
+
+}
---
+++ 1006.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main(){
+
+    double a, b, c, d;
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    scanf("%lf", &d);
+
+    c=((a*2)+(b*3)+(d*5))/10.0;
+
+    printf("MEDIA = %.1lf\n", c);
+    return 0;
+
+
+}
