67519095f140bec03bc052a65bfe8a7dcb3efe9c
oViniciusElias <vinicius.elias@unifesp.br>
2024-11-23 23:28:30 -0300

    Add files via upload

---
+++ 1070.c
@@ -0,0 +1,19 @@
+/*exercício para testar boa documentação, formatação e for*/
+
+#include <stdio.h>
+
+/*esse algoritimo recebe um valor inteiro e devolve os seis proximos numeros impares*/
+int main(){
+ 
+    int X;
+    scanf("%d", &X);
+    if (X % 2 !=0){
+        X--;}
+
+        for(int i=6; i>0; i--){
+            printf("%d\n",X+1);
+            X=X+2;}
+            
+    return 0;
+
+}
